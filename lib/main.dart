488b52374d8be95f956faaebf0513cb65330d89a
yasindu <yazindu@gmail.com>
2023-11-04 23:34:20 +0530

    feat: video 06 - Not a Widget Adding a Transaction Model & Transaction Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,24 +15,35 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'Gym membership', amount: 6000, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Saloon', amount: 300, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter App'),
       ),
-      body: Column(children: <Widget>[
-        Container(
-          width: double.infinity,
-          child: Card(
-            color: Colors.amberAccent,
-            child: Text('Chart!'),
-          ),
-        ),
-        Card(
-          child: Text('List of Transactions'),
-        ),
-      ]),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.amberAccent,
+                child: Text('Chart!'),
+              ),
+            ),
+            Card(
+              color: Colors.redAccent,
+              child: Text('List of Transactions'),
+            ),
+          ]),
     );
   }
 }
