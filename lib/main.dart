ab2f13a2bcc7edb6f92b3f4f9565c611ddedc837
yasindu <yazindu@gmail.com>
2023-11-05 17:51:55 +0530

    feat: 014 Installing External Packages & Formatting Dates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_expenses_app/transaction.dart';
 
 void main() => runApp(MyApp());
@@ -68,7 +69,7 @@ class MyHomePage extends StatelessWidget {
                                 fontWeight: FontWeight.bold, fontSize: 16),
                           ),
                           Text(
-                            transaction.date.toString(),
+                            DateFormat().add_yMMMd().format(transaction.date),
                             style: TextStyle(color: Colors.grey),
                           )
                         ],
